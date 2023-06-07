2f727bebe447fdfc43e3fa3d5cb1210dc23da6f6
Shahanaj2000 <shahanajmt@gmail.com>
2023-06-07 13:54:36 +0530

    Setting Up Firebase | Instagram |

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_Screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_screen_layout.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/util/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyDSPwzhItyxOU5hl5HcHaE1XuF3cz5Jecs",
+        appId: "1:125942513678:web:b66de3652ba75f5a713c5f",
+        messagingSenderId: "125942513678",
+        projectId: "125942513678",
+        storageBucket: "instagramapp-89166.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
--- lib/responsive/mobile_Screen_layout.dart
+++ lib/responsive/mobile_Screen_layout.dart
@@ -6,6 +6,7 @@ class MobileScreenLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       body: Center(
         child: Text("MobileScreen"),
       ),
