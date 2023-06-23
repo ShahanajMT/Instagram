67932c8b059310afde7afefe98e0f820b6d0c64c
Shahanaj2000 <shahanajmt@gmail.com>
2023-06-23 16:48:54 +0530

    Selecting Images | Instagram

--- lib/Views/UI/screens/add_post_screens.dart
+++ lib/Views/UI/screens/add_post_screens.dart
@@ -1,5 +1,9 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:instagram_flutter/util/colors.dart';
+import 'package:instagram_flutter/util/imagePicker.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -9,64 +13,133 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
+  Uint8List? _file;
+  final TextEditingController _captionComtrollers = TextEditingController();
+
+  //
+
+  //Selecting Images
+  _selectImage(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: const Text('Create a Post'),
+            children: [
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Take a Photo'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(ImageSource.camera);
+
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Choose a Photo from Gallery'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(ImageSource.gallery);
+
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Cancel'),
+                onPressed: ()  {
+                  Navigator.of(context).pop();
+                 
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _captionComtrollers.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: mobileBackgroundColor,
-          leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.arrow_back),
-          ),
-          title: const Text('New Post'),
-          centerTitle: false,
-          actions: [
-            TextButton(
-              onPressed: () {},
-              child: const Text(
-                'Share',
-                style: TextStyle(
-                  color: Colors.blueAccent,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+    return _file == null
+        ? Center(
+            child: IconButton(
+              onPressed: () => _selectImage(context),
+              icon: const Icon(Icons.upload_file_outlined),
             ),
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 17),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Container(
-                    height: 100,
-                    width: 75,
-                    color: Colors.red,
-                  ),
-                  const SizedBox(
-                    width: 12,
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.3,
-                    child: const TextField(
-                      decoration: InputDecoration(
-                          hintText: 'write a caption....',
-                          border: InputBorder.none,),
-                      //maxLines: 8,
+          )
+        : SafeArea(
+            child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: mobileBackgroundColor,
+                leading: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.arrow_back),
+                ),
+                title: const Text('New Post'),
+                centerTitle: false,
+                actions: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Share',
+                      style: TextStyle(
+                        color: Colors.blueAccent,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ],
               ),
-              const SizedBox(
-                height: 20,
+              body: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 17),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          height: 100,
+                          width: 75,
+                          child: Image(image: MemoryImage(_file!), ),
+                        ),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.3,
+                          child:  TextField(
+                            controller: _captionComtrollers,
+                            decoration: const InputDecoration(
+                              hintText: 'write a caption....',
+                              border: InputBorder.none,
+                            ),
+                            //maxLines: 8,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
